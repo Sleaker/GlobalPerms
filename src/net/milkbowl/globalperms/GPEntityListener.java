287cd6f72a927513b8772da1c9398688f82f25b4
Sleaker <sleaker@gmail.com>
2011-11-16 22:41:00 -0800

    updated to latest CB dependency

--- src/net/milkbowl/globalperms/GPEntityListener.java
+++ src/net/milkbowl/globalperms/GPEntityListener.java
@@ -1,8 +1,8 @@
 package net.milkbowl.globalperms;
 
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Projectile;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
-import org.bukkit.event.entity.EntityDamageByProjectileEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityListener;
 
@@ -15,48 +15,42 @@ public class GPEntityListener extends EntityListener {
 
 	@Override
 	public void onEntityDamage(EntityDamageEvent event) {
-		if (event.isCancelled())
+		if (event.isCancelled() || !(event instanceof EntityDamageByEntityEvent))
 			return;
 
+		EntityDamageByEntityEvent subEvent = (EntityDamageByEntityEvent) event;
 		Player player = null;
 		if (event.getEntity() instanceof Player) {
 			Player defender = (Player) event.getEntity();
 			Player attacker = null;
-			if (event instanceof EntityDamageByEntityEvent) {
-				EntityDamageByEntityEvent subEvent = (EntityDamageByEntityEvent) event;
-				if (!(subEvent.getDamager() instanceof Player)) 
-					return;
-
-				else
-					attacker = (Player) subEvent.getDamager();
-
-			} else if (event instanceof EntityDamageByProjectileEvent) {
-				EntityDamageByProjectileEvent subEvent = (EntityDamageByProjectileEvent) event;
-				if (!(subEvent.getDamager() instanceof Player)) 
+			if (subEvent.getDamager() instanceof Projectile) {
+				if (!(((Projectile) subEvent.getDamager()).getShooter() instanceof Player))
 					return;
 				else
-					attacker = (Player) subEvent.getDamager();
-			}
+					attacker = (Player) ((Projectile) subEvent.getDamager()).getShooter();
+			} else if (!(subEvent.getDamager() instanceof Player)) 
+				return;
 
-			if (attacker != null) {
-				if ( plugin.has(defender, Perms.PVP_IMMUNE) || plugin.has(attacker, Perms.PVP_IMMUNE) )
-					event.setCancelled(true);
-			}
-		} else if (event instanceof EntityDamageByEntityEvent) {
-			EntityDamageByEntityEvent subEvent = (EntityDamageByEntityEvent) event;
-			if (subEvent.getDamager() instanceof Player) {
-				player = (Player) subEvent.getDamager();
-			} else if (subEvent.getEntity() instanceof Player)
-				player = (Player) subEvent.getEntity();
-		} else if (event instanceof EntityDamageByProjectileEvent) {
-			EntityDamageByProjectileEvent subEvent = (EntityDamageByProjectileEvent) event;
-			if (subEvent.getDamager() instanceof Player) {
-				player = (Player) subEvent.getDamager();
-			} else if (subEvent.getEntity() instanceof Player)
-				player = (Player) subEvent.getEntity();
-		}
-		if (player != null)
-			if (plugin.has(player, Perms.COMBAT_IMMUNE))
+			else
+				attacker = (Player) subEvent.getDamager();
+
+
+			if (attacker != null && (plugin.has(defender, Perms.PVP_IMMUNE) || plugin.has(attacker, Perms.PVP_IMMUNE))) {
 				event.setCancelled(true);
+				return;
+			}
+		} 
+		
+		if (subEvent.getDamager() instanceof Player) {
+			player = (Player) subEvent.getDamager();
+		} else if (subEvent.getEntity() instanceof Player)
+			player = (Player) subEvent.getEntity();
+		else if (subEvent.getEntity() instanceof Projectile) {
+			if (((Projectile) subEvent.getEntity()).getShooter() instanceof Player)
+				player = (Player) ((Projectile) subEvent.getEntity()).getShooter();
+		}
+
+		if (player != null && plugin.has(player, Perms.COMBAT_IMMUNE))
+			event.setCancelled(true);
 	}
 }
