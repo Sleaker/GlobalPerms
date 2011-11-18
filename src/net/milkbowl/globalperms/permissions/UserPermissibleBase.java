8a691f272b445a1f8207fb207f733e3542b489e5
Sleaker <sleaker@gmail.com>
2011-11-17 23:16:27 -0800

    initial PermissibleBase override

--- src/net/milkbowl/globalperms/permissions/UserPermissibleBase.java
+++ src/net/milkbowl/globalperms/permissions/UserPermissibleBase.java
@@ -1,34 +1,87 @@
 package net.milkbowl.globalperms.permissions;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.permissions.Permissible;
 import org.bukkit.permissions.PermissibleBase;
 import org.bukkit.permissions.Permission;
-import org.bukkit.permissions.ServerOperator;
 
+/**
+ * Designed to be injected over a player's PermissibleBase (overriding it)
+ *
+ */
 public class UserPermissibleBase extends PermissibleBase {
 
-	public UserPermissibleBase(ServerOperator opable) {
-		super(opable);
+	public UserPermissibleBase(Player player) {
+		super(player);
+	}
+
+	public static void inject(Player player) {
+		Permissible permissible = new UserPermissibleBase(player);
+
+		try {
+			if (player.getClass().getName().contains("Spout")) {
+				injectSpout(player, permissible);
+			} else {
+				injectCraftBukkit(player, permissible);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	protected static void injectCraftBukkit(Player player, Permissible permissible) throws Exception {
+		Class<?> humanEntity = Class.forName("org.bukkit.craftbukkit.entity.CraftHumanEntity");
+
+		Field permField = humanEntity.getDeclaredField("perm");
+		// Make it public for reflection
+		permField.setAccessible(true);
+
+		PermissibleBase oldBase = (PermissibleBase) permField.get(player);
+
+		// Copy permissions and attachments from old Permissible
+
+		// Attachments
+		Field attachmentField = PermissibleBase.class.getDeclaredField("attachments");
+		attachmentField.setAccessible(true);
+		attachmentField.set(permissible, attachmentField.get(oldBase));
+
+		// Permissions
+		Field permissionsField = PermissibleBase.class.getDeclaredField("permissions");
+		permissionsField.setAccessible(true);
+		permissionsField.set(permissible, permissionsField.get(oldBase));
+
+		// Inject permissible
+		permField.set(player, permissible);
+	}
+
+	protected static void injectSpout(Player player, Permissible permissible) throws Exception {
+		Class<?> humanEntity = Class.forName("org.getspout.spout.player.SpoutCraftPlayer");
+		Field permField = humanEntity.getDeclaredField("perm");
+		permField.setAccessible(true);
+
+		permField.set(player, permissible);
 	}
 
 	public boolean inGroup(String group) {
 		if (group == null)
 			throw new IllegalArgumentException("Group name cannot be null");
-		
+
 		Permission perm = Bukkit.getServer().getPluginManager().getPermission(group);
 		if (!(perm instanceof Group))
 			return false;
-		
+
 		return hasPermission(group);
 	}
-	
+
 	public boolean inGroup(Group group) {
 		return inGroup(group.getName());
 	}
-	
+
 	public List<Group> getGroups() {
 		List<Group> groups = new ArrayList<Group>();
 		/*	Loop through all Groups and check if inGroup - add to list if true
